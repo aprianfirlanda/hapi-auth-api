e729b8eea93c50e0c8874979488f1f0f46a958dd
aprianfirlanda <aprianfirlanda@gmail.com>
2023-02-03 06:51:31 +0700

    add createServer

---
+++ src/Infrastructures/http/createServer.js
@@ -0,0 +1,27 @@
+const Hapi = require('@hapi/hapi');
+
+const createServer = async (container) => {
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([]);
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hi, welcome to auth api',
+    }),
+  });
+
+  return server;
+};
+
+module.exports = createServer();
